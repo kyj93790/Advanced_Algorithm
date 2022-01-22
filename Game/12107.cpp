111b6ce76aba8886a3f7a9471dfc1ae16f76e84b
kyj93790 <kyj93790@naver.com>
2022-01-22 20:20:47 +0900

    약수 지우기 게임 1

---
+++ Game/12107.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+int main(void) {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    ll n;
+    cin >> n;
+    if (n==1) cout << "B\n";
+    else cout << "A\n";
+    return 0;
+}
