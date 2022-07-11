6e7b138c1222cbee0846525653ccabf54514d42a
kyj93790 <kyj93790@naver.com>
2022-07-11 13:21:57 +0900

    N과 M(1)

---
+++ 2022-S/분할정복/15649.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+#define p(a, b) make_pair(a, b)
+
+typedef long long ll;
+ll n, m, choose;
+bool visit[9];
+vector<int> v;
+
+void f() {
+    if (choose == m) {
+        for (int i=0; i<v.size(); i++) {
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+        return ;
+    }
+    for (int i=1; i<=n; i++) {
+        if (!visit[i]) {
+            v.push_back(i);
+            visit[i] = true;
+            choose++;
+            f();
+            v.pop_back();
+            visit[i] = false;
+            choose--;
+        }
+    }
+}
+
+int main(void) {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    cin >> n >> m;
+    f();
+    return 0;
+}
